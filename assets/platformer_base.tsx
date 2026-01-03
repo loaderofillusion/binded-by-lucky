04f0b733b1d1927823f5ec49e3162fd422d2c464
loaderofillusion <art.mizirov@mail.ru>
2026-01-03 21:00:35 +0500

    добавлено использование Tiled. исправлены баги передвижения связанные с рывком и техникой рывка, улучшена энерция

---
+++ assets/platformer_base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformer_base" tilewidth="16" tileheight="16" spacing="2" tilecount="2600" columns="50">
+ <image source="../../Base pack/Tiles/tiles_spritesheet.png" width="914" height="936"/>
+</tileset>
